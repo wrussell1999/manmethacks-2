f8ee42bc85e278945314d8b2f3fcfec2dc196531
Natalie Smith <natalie.smith@tjcn.co.uk>
2020-01-26 03:18:35 +0000

    Add play, record, and library buttons

---
+++ app/src/main/java/com/willrussell/manmethacks2/LibraryActivity.kt
@@ -0,0 +1,12 @@
+package com.willrussell.manmethacks2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LibraryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library)
+    }
+}
--- app/src/main/java/com/willrussell/manmethacks2/MainActivity.kt
+++ app/src/main/java/com/willrussell/manmethacks2/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.willrussell.manmethacks2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openRecordings(v: View?) {
+        val intent = Intent(applicationContext, LibraryActivity::class.java)
+        startActivity(intent)
     }
 }
